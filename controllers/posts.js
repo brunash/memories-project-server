2824e0710e94e9bac82c80650c3e1c28a62c40fb
brunash <bobazso@yahoo.com>
2022-05-02 16:41:51 -0400

    added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../modals/postMessage.js";
 
 
@@ -24,4 +25,15 @@ export const createPosts = async (req, res) => {
         res.status(409).json ({message: error.message});
 
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+    res.json(updatedPost);
 }
